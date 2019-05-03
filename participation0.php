f5565be7521b09c62971c2bcb15ddf1c299a426d
elisabettagabriele <46170718+elisabettagabriele@users.noreply.github.com>
2019-05-03 18:25:37 -0400

    Update participation0.php

--- participation0.php
+++ participation0.php
@@ -1,3 +1,18 @@
+<?php
+
+
+    session_start(); 
+    if (!isset($_SESSION['count'])){ 
+     $_SESSION['count'] = 1; 
+    }
+    else{  
+     $_SESSION['count']++;
+    }
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 	
@@ -20,41 +35,17 @@
 
                         Made by students for students
                         
-                        Join the party and participate.</p>
-        </div>
-
-        <fieldset class = "leaving">
-				
-				<div>
-                <h3 id ="title">Enter your information and I can get back to you!</h3>
-					Name: <input type="text" name="names" />
-					<span class="error">* <?php echo $namesErr;?></span>
-				</div>
-
-				<div>
-					Email: <input type="text" name="email"/>
-					<span class="error">* <?php echo $emailErr;?></span>
-                </div>
                 
-			<input id = "query" type="submit" class="submit"/>
-            </fieldset>
-            
-
+                        Join the party and participate. 
+                    <br>Join the party <a href = "participation1.html"> here </a>.
+                
+                        Professors can host the party <a href = "participation2.html"> here </a>.
+            </p>
     
-        
-        
-        
-        </div>
-		
-                   
-        
-
-
-
-
-<?php
-
-            // define variables and set to empty values 
+         </div>
+            <?php
+            
+             // define variables and set to empty values 
             $names = $email = " ";
             $namesErr = $emailErr = " ";
 
@@ -76,7 +67,7 @@
     
     
             if (empty($_POST["email"])){
-                $emailErr = "error, your email name is empty";
+                $emailErr = "Email is empty";
 
         }
             else{
@@ -91,3 +82,51 @@
         }
 
 ?>
+            <form id = "f" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+
+			<fieldset  class = "leaving">
+				<legend id ="title">Enter your information and I can get back to you!</legend>
+				<div class = "divy">
+					Name: <input id="thename" type="text" name="names" required />
+					<span class="error">* <?php echo $namesErr;?></span>
+				</div>
+
+				<div class = "divy">
+					Email: <input id="thename2" type="text" name="email" required/>
+					<span class="error">* <?php echo $emailErr;?></span>
+				</div>
+                
+                <div class = "divy">
+                    Concerns: <input class="more" type = "text" name = "texxt">
+                </div>
+			</fieldset>
+			
+            
+			
+			
+			<input id = "query" type="submit" />
+		</form>
+            
+            
+<?php
+        
+echo "<h2>Thanks for your info:</h2>";
+         $th = $_POST["texxt"];
+?> 
+       
+<div class="container1"> <?php echo $names;?> <br> <?php echo $email;?> <br> <?php echo $th;?> </div>
+<?php
+echo "<br>";
+
+
+        
+
+echo "<br>";
+
+?>
+            
+       
+    </body>
+</html>
+		
+              
